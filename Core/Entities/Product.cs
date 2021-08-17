462bb65491c323045d0bdafe9c488135cdee39d8
Nathan Daniel <46191474+zewdnehnathan@users.noreply.github.com>
2021-08-17 23:11:26 +0300

    Add project files.

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; protected set; }
+
+        public string Name { get; protected set; }
+
+
+
+    }
+}
